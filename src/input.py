442197b9996ffdd90bd3fee2b69a10079c2ac09f
brycerr <94137678+brycerr@users.noreply.github.com>
2025-03-10 20:19:21 -0500

    Refactor default cell size behavior

--- src/input.py
+++ src/input.py
@@ -7,17 +7,6 @@ def get_map_data_input():
     if map_name == "":  # default test value
         map_name = "test"
 
-    # TODO: input validation/error handling
-    input_map_width_cells = input("Input map width (west/east) in cells: ")
-    if not input_map_width_cells:   # default test value
-        input_map_width_cells = 40
-    map_width_cells = int(input_map_width_cells)
-
-    input_map_height_cells = input("Input map height (north/south) in cells: ")
-    if not input_map_height_cells:  # default test value
-        input_map_height_cells = 40
-    map_height_cells = int(input_map_height_cells)
-
     # TODO: better coordinate selection: potentially drop a point on a map?
     # TODO: input validation/error handling
     input_lat = input("Input latitude: ")
@@ -34,6 +23,19 @@ def get_map_data_input():
     if not input_radius:    # default test value
         input_radius = 12000
     radius = int(input_radius)
+
+    # TODO: input validation/error handling
+    cell_size = 300     # TODO: move this to a global enum
+
+    input_map_width_cells = input("Input map width (west/east) in cells: ")
+    if not input_map_width_cells:   # default test value
+        input_map_width_cells = round(radius / cell_size)
+    map_width_cells = int(input_map_width_cells)
+
+    input_map_height_cells = input("Input map height (north/south) in cells: ")
+    if not input_map_height_cells:  # default test value
+        input_map_height_cells = round(radius / cell_size)
+    map_height_cells = int(input_map_height_cells)
 
     map_data = [map_name, map_width_cells, map_height_cells, lat, lon, radius]
 
