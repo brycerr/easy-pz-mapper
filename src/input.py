58e49a05994a32148612f1b83869c647d014a6a0
brycerr <94137678+brycerr@users.noreply.github.com>
2025-04-07 01:23:30 -0500

    Set default values

--- src/input.py
+++ src/input.py
@@ -11,17 +11,17 @@ def get_map_data_input():
     # TODO: input validation/error handling
     input_lat = input("Input latitude: ")
     if not input_lat:   # default test value
-        input_lat = 43.2
+        input_lat = 42.83
     lat = float(input_lat)
 
     input_lon = input("Input longitude: ")
     if not input_lon:   # default test value
-        input_lon = -88.716667
+        input_lon = -88.74
     lon = float(input_lon)
 
     input_radius = input("Input radius (meters): ")
     if not input_radius:    # default test value
-        input_radius = 12000
+        input_radius = 3000
     radius = int(input_radius)
 
     # TODO: input validation/error handling
@@ -40,3 +40,34 @@ def get_map_data_input():
     map_data = [map_name, map_width_cells, map_height_cells, lat, lon, radius]
 
     return map_data
+
+
+def select_mode():
+    """Allows user to select to load stored data or to query Overpass for new data."""
+    selection_prompt = "Select Mode:\n" \
+                       " [0] Exit\n" \
+                       " [1] Load Stored Map Data\n" \
+                       " [2] Generate New Map Data\n"
+    print(selection_prompt)
+
+    mode = int(input("Input option number: "))
+    while 0 > mode or mode > 2:
+        print("Invalid input. Please type the number of the mode you would like to select.")
+        print(selection_prompt)
+
+        mode = int(input("Input option number: "))
+
+    if mode == 0:
+        print("Exiting program.")
+        exit()
+    elif mode == 1:
+        # TODO: load data
+        print("Selected: ")
+        pass
+    elif mode == 2:
+        print("Selected: Generate New Map Data\n")
+        return get_map_data_input()
+
+
+if __name__ == "__main__":
+    select_mode()
